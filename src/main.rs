b6bcc68bd7f2fd600d32a708671cbc17af44b59e
Ember Hext <emberhext@gmail.com>
2023-04-06 09:10:08 -0400

    Added license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+/*
+ * HARVEST (High-speed Asynchronous Rust Vocabulary Extraction and Search Tool) is a tool to crawl through a
+ * supplied web page, and 
+ * 
+ * Author: Ember Hext
+ * GitHub: https://github.com/EmberHext
+ * Twitter: @EmberHext
+ * 
+ * It is released under the MIT License:
+ * 
+ * Copyright 2023 Ember Hext
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the “Software”), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * 
+ * Note: This is intended to be used for your own servers or in contexts like pentesting and CTFs where you are authorised 
+ * to be engaging with the server in this manner.
+ * 
+ */
+
 fn main() {
     println!("Hello, world!");
 }
